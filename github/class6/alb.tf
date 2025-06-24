5d4e87a65aed179305c363684eafbe0e716e9797
Sajithasafrin <sajitha0430@gmail.com>
2025-06-24 19:45:40 +0000

    final1

--- github/class6/alb.tf
+++ github/class6/alb.tf
@@ -73,8 +73,8 @@ resource "aws_lb_listener" "https_forward" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate.cert.arn
+  #ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  #certificate_arn   = aws_acm_certificate.cert.arn
 
   default_action {
     type             = "forward"
